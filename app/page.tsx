1d86844ea7b38997165981b9b6b70d374bbbfd46
Syed zain ali shah <zaingit780@gmail.com>
2023-10-10 05:52:13 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,13 @@ import { OrbitControls } from "@react-three/drei/core";
 export default function Home() {
   return (
     <>
-      <Canvas style={{ background: "#171717", height: "100vh" }}>
-        <ambientLight intensity={1} />
+      <Canvas
+        style={{ background: "#171717", height: "100vh" }}
+        camera={{ fov: 35 }}
+      >
+        <ambientLight intensity={2} />
         <spotLight
-          intensity={0.5}
+          intensity={2.5}
           angle={0.1}
           penumbra={1}
           position={[10, 15, 10]}
